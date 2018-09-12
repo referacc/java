900cc13389c2db370c143457549410337c244e99
referacc <contact.sentinent@gmail.com>
2018-09-12 20:29:49 +0100

    Input basics of Java

---
+++ src/me/ref/Main.java
@@ -0,0 +1,20 @@
+package me.ref;
+
+/**
+ * String - Text
+ * int - Two's compliment integer (16 bits)
+ * boolean - True or false
+ * double - IEEE 754 floating point representation (64 bits)
+ * long - Two's compliment integer (64 bits)
+ * short - IEEE 754 floating point representation (8 bits)
+ * byte - -1 <- 0 -> 1
+ * char - Unicode character
+ * float - IEEE 754 floating point representation (32 bits)
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
